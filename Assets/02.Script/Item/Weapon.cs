b6b237f93af7dc14285900d7506a64489752d820
JongHyeon <winin1216@gmail.com>
2024-11-11 12:10:15 +0900

    hinggu2

--- Assets/02.Script/Item/Weapon.cs
+++ Assets/02.Script/Item/Weapon.cs
@@ -5,7 +5,7 @@ public class Weapon : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+        Debug.Log("djdikfjdksf");
     }
 
     // Update is called once per frame
